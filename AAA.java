42f30367e7a8cceee153c7a03073447b52acdf5e
jpsalazart <45083808+jpsalazart@users.noreply.github.com>
2018-11-15 22:00:29 -0300

    Inicio_Repositorio

---
+++ AAA.java
@@ -0,0 +1,24 @@
+package kkkkk;
+
+public class AAA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String alimento=" manzana ";
+	String nombre="jason";
+	int tin=5;
+	
+	
+	System.out.println(alimento.trim());
+	System.out.println(nombre.toString());
+	System.out.println(alimento.length());
+	System.out.println(nombre.hashCode());
+	System.out.println(charAt(tin));
+	
+	
+	
+		
+	}
+
+}
+;
